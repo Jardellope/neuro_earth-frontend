42cd4674d5ccdac69d306ae87c6dba889b1c1570
Jardellope <112778480+Jardellope@users.noreply.github.com>
2025-09-28 12:59:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-// Conectar ao backend no Render
+// Conectar rota / (status)
+fetch("https://euro-earth-backend.onrender.com/")
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("status").innerText = data.status;
+  });
+
+// Conectar rota /visao
 fetch("https://euro-earth-backend.onrender.com/visao")
-  .then(response => response.json())
+  .then(res => res.json())
   .then(data => {
     document.getElementById("missao").innerText = data.missao;
     document.getElementById("objetivo").innerText = data.objetivo;
-  })
-  .catch(err => console.error("Erro ao carregar API:", err));
+  });
+
+// Conectar rota /clima
+fetch("https://euro-earth-backend.onrender.com/clima")
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("clima").innerText =
+      `${data.alerta} | Temp: ${data.dados.temperatura} | Umidade: ${data.dados.umidade}`;
+  });
